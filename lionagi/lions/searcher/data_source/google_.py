65dec7cbb8ed944b6076a608d234230e30348dfe
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-04-20 12:28:15 -0400

    searcher initial commit

--- lionagi/lions/searcher/data_source/google_.py
+++ lionagi/lions/searcher/data_source/google_.py
@@ -1,11 +1,10 @@
 import os
 
-google_key_scheme = "GOOGLE_API_KEY"
-google_engine_scheme = "GOOGLE_CSE_ID"
+google_key_scheme = 'GOOGLE_API_KEY'
+google_engine_scheme = 'GOOGLE_CSE_ID'
 
 google_api_key = os.getenv(google_key_scheme)
 google_engine = os.getenv(google_engine_scheme)
-
 
 class GoogleSearch:
     """
@@ -25,15 +24,16 @@ class GoogleSearch:
         search(title: str = None, url: str = None):
             Perform a Google search for a given title or URL and retrieve relevant content.
     """
-
     api_key = google_api_key
     search_engine = google_engine
-    search_url = """
+    search_url = (
+        """
         https://www.googleapis.com/customsearch/v1?key={key}&cx={engine}&q={query}&start={start}
         """
+        )
 
     @classmethod
-    def create_agent_engine(cls, api_key=None, search_engine=None, verbose=False):
+    def create_agent(cls, api_key=None, search_engine=None, verbose=False):
         """
         Create an OpenAIAgent for Google search operations.
 
@@ -49,11 +49,31 @@ class GoogleSearch:
             ImportError: If there is an issue during the agent creation.
         """
         try:
-            from llama_index.agent import OpenAIAgent
-            from llama_index.tools.tool_spec.load_and_search.base import (
-                LoadAndSearchToolSpec,
+            from lionagi.libs import SysUtil
+            SysUtil.check_import(
+                package_name="llama_index",
+                pip_name="llama-index"
             )
-            from llama_hub.tools.google_search.base import GoogleSearchToolSpec
+            
+            SysUtil.check_import(
+                package_name="llama_index",
+                module_name="agent.openai",
+                import_name="OpenAIAgent",
+                pip_name="llama-index-agent-openai"
+            )
+            
+            SysUtil.check_import(
+                package_name="llama_index",
+                module_name="tools.google",
+                import_name="GoogleSearchToolSpec",
+                pip_name="llama-index-tools-google"
+            )
+            
+            from llama_index.agent import OpenAIAgent
+            from llama_index.tools.tool_spec.load_and_search.base import LoadAndSearchToolSpec
+            from llama_index.tools.google import GoogleSearchToolSpec
+            from llama_index.llms.openai import OpenAI
+            llm = OpenAI(model="gpt-4-turbo", temperature=0.1)
 
             api_key = api_key if api_key else cls.api_key
             search_engine = search_engine if search_engine else cls.search_engine
@@ -65,45 +85,70 @@ class GoogleSearch:
             ).to_tool_list()
 
             # Create the Agent with our tools
-            agent = OpenAIAgent.from_tools(tools, verbose=verbose)
+            agent = OpenAIAgent.from_tools(tools, verbose=verbose, llm=llm)
             return agent
-
+        
         except Exception as e:
             raise ImportError(f"Error in importing OpenAIAgent from llama_index: {e}")
 
-    @staticmethod
-    def search(title: str = None, url: str = None):
-        """
-        Perform a Google search for a given title or URL and retrieve relevant content.
 
-        Args:
-            title (str, optional): The title or query for the Google search. Defaults to None.
-            url (str, optional): The URL to retrieve content from. Defaults to None.
+# responses_google = []
+# async def query_google(query: str):
+#     """
+#     Search Google and retrieve a natural language answer to a given query.
 
-        Returns:
-            str: The retrieved information from the Google search or URL, or an error message if no results are found.
+#     Args:
+#         query (str): The search query to find an answer for.
 
-        Raises:
-            ValueError: If there is an issue during the search or content retrieval.
-        """
-        if not title and not url:
-            raise "No search input."
-        from ..utils.url_util import get_url_content
+#     Returns:
+#         str: A natural language answer obtained from Google search results.
 
-        if url:
-            return get_url_content(url)
-        else:
-            from googlesearch import search
+#     Raises:
+#         Exception: If there is an issue with making the request or parsing the response.
+#     """
+#     google_agent = GoogleSearch.create_agent()
+#     response = await google_agent.achat(query)
+#     responses_google.append(response)
+#     return str(response.response)
 
-            search_result = search(title)
 
-            for url in search_result:
-                try:
-                    return get_url_content(url)
 
-                except:
-                    continue
-            return "No matched or valid source"
+
+
+    # @staticmethod
+    # def search(title: str = None, url: str = None):
+    #     """
+    #     Perform a Google search for a given title or URL and retrieve relevant content.
+
+    #     Args:
+    #         title (str, optional): The title or query for the Google search. Defaults to None.
+    #         url (str, optional): The URL to retrieve content from. Defaults to None.
+
+    #     Returns:
+    #         str: The retrieved information from the Google search or URL, or an error message if no results are found.
+
+    #     Raises:
+    #         ValueError: If there is an issue during the search or content retrieval.
+    #     """
+    #     if not title and not url:
+    #         raise 'No search input.'
+    #     from ..utils.url_util import get_url_content
+
+    #     if url:
+    #         return get_url_content(url)
+    #     else:
+    #         from googlesearch import search
+    #         search_result = search(title)
+
+    #         for url in search_result:
+    #             try:
+    #                 return get_url_content(url)
+
+    #             except:
+    #                 continue
+    #         return 'No matched or valid source'
+
+
 
     # get fields of a google search item
     # @classmethod
